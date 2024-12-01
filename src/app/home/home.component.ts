875a9df28649191e620bb7f8879ad586dcef94e9
hagansamuel6 <hagansamuel6@gmail.com>
2024-12-01 13:33:10 +0000

    filtering

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,10 +13,23 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingLocationSingle: HousingLocation;
+  filteredLocationList: HousingLocation[];
 
   // housingService : HousingService = Inject(HousingService);
   constructor(private housingService : HousingService){
     this.housingLocationList = this.housingService.getAllHousingLocations();
     this.housingLocationSingle = this.housingService.getHousingLocation();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string){
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
